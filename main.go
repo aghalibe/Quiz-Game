9662d9a08ea374da77d1a1eec2ca7765b081bdd7
chiamaka <aghalibetoch@gmail.com>
2025-01-21 10:31:40 +0100

    add csv file and updated quiz code

--- main.go
+++ main.go
@@ -9,13 +9,8 @@ import (
 
 // define flags and tell flag package to parse all of them
 
-func exit(msg string) {
-	fmt.Println(msg)
-	os.Exit(1)
-}
-
 func main() {
-	csvFilename := flag.String("csv", "questions.csv", "a csv file in the format of 'problem,solution'")
+	csvFilename := flag.String("csv", "questions.csv", "a csv file in the format of 'question,solution'")
 	flag.Parse()
 
 	file, err := os.Open(*csvFilename)
@@ -29,6 +24,42 @@ func main() {
 	if err != nil {
 		exit("Failed to parse the provided CSV File. ")
 	}
-	fmt.Println(lines)
+	questions := parseLines(lines)
+	//fmt.Println(questions)
 
+	//keep count of how many questions are correct
+	correct := 0
+	for i, p := range questions {
+		fmt.Printf("question #%d: %s = \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+	fmt.Printf("You scored %d out of %d.\n", correct, len(questions))
+}
+
+//build a question via csv 2d slice
+
+func parseLines(lines [][]string) []question {
+	ret := make([]question, len(lines))
+
+	for i, line := range lines {
+		ret[i] = question{
+			q: line[0],
+			a: line[1],
+		}
+	}
+	return ret
+}
+
+type question struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
